56ae1221e18765d988f37b934fc40388d2211e77
willychang21 <willychang17@gmail.com>
2023-02-10 18:13:58 -0800

    mp1

--- helloWorld/ViewController.swift
+++ helloWorld/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var btn: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func clickBtn(_ sender: Any) {
+        label.text = "Hello World"
+    }
+    
 }
 
